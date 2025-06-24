5e6bb2b7ed1f11f0d033ef81e98030ce60e08b4e
Diego Forero <43680183+diego-forero@users.noreply.github.com>
2025-06-23 23:36:22 -0500

    use .js

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import catalogRoutes from './routes/catalog';
+import catalogRoutes from './routes/catalog.js';
 
 dotenv.config();
 const app = express();
